7c3bc75c3238243fec4eccb8547e114252718559
Erick Ayala <erickayalrias@gmail.com>
2022-02-23 12:37:27 +0100

    routerrr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  
+  return (
+  <Routes>
+    <Route path="/" element={<Home /> }
+  </Routes> )
 }
 
 export default App;
